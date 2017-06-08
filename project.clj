2314978534095e94bb50ef9ab0f69efc16bec5d9
chpill <etienne@spillemaeker.com>
2017-06-08 08:27:51 +0200

    Add leiningen entry point

---
+++ project.clj
@@ -0,0 +1,7 @@
+(defproject local-4clj "0.1.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :url "https://github.com/chpill/local-4clj"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.8.0"]]
+  :plugins [[org.clojars.chpill/lein-nightlight "1.6.5-SNAPSHOT"]])
